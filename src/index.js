f9f43f27a67234507f910c9d7cb4a8e6bf8dd112
Yury Maniakhin <manah9191@mail.ru>
2023-08-13 16:07:21 +0300

    feat: added  a second task solution

--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
 module.exports = function check(str, bracketsConfig) {
     if (str.length % 2 > 0) return false;
+    
+    const OPEN_BRACKETS = [];
+    const BRACKETS_PAIRS = {};
 
-    let temp = str;
+    bracketsConfig.forEach(bracket => {
+        OPEN_BRACKETS.push(bracket[0]);
+        BRACKETS_PAIRS[bracket[1]] = bracket[0];
+    })
+
+    const stack = [];
+
 
     for (let i = 0; i < str.length; i++) {
-        for (let j = 0; j < bracketsConfig.length; j++) {
+        let currentSymbol = str[i];
 
-            temp = temp.replace(bracketsConfig[j][0] + bracketsConfig[j][1], '');
+        if (OPEN_BRACKETS.includes(currentSymbol)) {
 
-            if (temp.length == 0) {
-                return true;
+            if (BRACKETS_PAIRS[currentSymbol] == stack[stack.length - 1] && stack.length != 0) {
+                stack.pop();
+            } else {
+                stack.push(currentSymbol);
             }
 
+        } else {
+
+            if (stack.length == 0) {
+                return false;
+            }
+
+            if (BRACKETS_PAIRS[currentSymbol] == stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                return false;
+            }
         }
     }
-    return false;
+
+    return stack.length == 0;
+
+
+    // if (str.length % 2 > 0) return false;
+
+    // let temp = str;
+
+    // for (let i = 0; i < str.length; i++) {
+    //     for (let j = 0; j < bracketsConfig.length; j++) {
+
+    //         temp = temp.replace(bracketsConfig[j][0] + bracketsConfig[j][1], '');
+
+    //         if (temp.length == 0) {
+    //             return true;
+    //         }
+
+    //     }
+    // }
+    // return false;
 }
